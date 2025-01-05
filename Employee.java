563550d0c7332af28db7c803cedff47107431c8f
Danil-Si <144678368+Danil-Si@users.noreply.github.com>
2025-01-05 12:38:45 +0300

    Add files via upload
    L4. Парадигма ООП

---
+++ Employee.java
@@ -0,0 +1,37 @@
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    // Конструктор
+    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    // Метод для вывода информации
+    public void printInfo() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+        System.out.println();
+    }
+
+    // Геттер для возраста
+    public int getAge() {
+        return age;
+    }
+}
+
+
